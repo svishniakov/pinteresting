5153c74898b4a09879ece540df09d55665e8e2ba
Sergey Vishniakov <svishniakov@gmail.com>
2014-05-22 14:17:54 +0200

    Paperclip configuration for AWS and Heroku

--- config/initializers/paperclip.rb
+++
@@ -1,3 +0,0 @@
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = 'pinzz.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
