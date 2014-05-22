84ba9bceab657cee00b5c79c40eefef597e5b661
Sergey Vishniakov <svishniakov@gmail.com>
2014-05-22 17:12:12 +0200

    Pagination "per_page" parameter changed to 20

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
-    @pins = Pin.all.order("updated_at DESC").paginate(:page => params[:page], :per_page => 3)
+    @pins = Pin.all.order("updated_at DESC").paginate(:page => params[:page], :per_page => 20)
   end
 
   def show
