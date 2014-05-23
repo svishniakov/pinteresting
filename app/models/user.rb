5b3b2760ce8f74923ce1adb420f63da6be592681
Sergey Vishniakov <svishniakov@gmail.com>
2014-05-23 13:36:57 +0200

    Fixing a bug with deleted user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :pins
+  has_many :pins, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true, length: { minimum: 2, maximum: 25, too_long: "%{count} characters is the maximum allowed" }
 end
